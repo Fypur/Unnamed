13c1baa69402b5d9d6a7ded5666af6711d6462aa
Fypur <84095553+Fypur@users.noreply.github.com>
2021-11-14 20:04:42 +0100

    Beggining Moving Platform

---
+++ Actors/MovingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Basic_platformer.Actors
+{
+    public class MovingPlatform : Actor
+    {
+        Vector2[] positions;
+        float[] times;
+
+        public MovingPlatform(Vector2 position, int width, int height, Vector2[] Otherpositions, float[] timesBetweenPositions)
+            : base(position, width, height, 0)
+        {
+            Contract.Requires(timesBetweenPositions.Length - 1 == Otherpositions.Length);
+            positions = Otherpositions;
+        }
+        
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Render()
+        {
+            base.Render();
+        }
+    }
+}
