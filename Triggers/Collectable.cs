b403b55949919308a0369fbd25d69155b3a4c914
Fypur <84095553+Fypur@users.noreply.github.com>
2022-02-05 14:05:02 +0100

    Collectable and Player Trigger classes

---
+++ Triggers/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_platformer
+{
+    public abstract class Collectable : PlayerTrigger
+    {
+        private bool collected;
+
+        public Collectable(Vector2 position, int width, int height, Sprite sprite)
+            : base(position, width, height, sprite) { }
+
+        public override void OnTriggerEnter(Entity entity)
+        {
+            if (!collected)
+            {
+                collected = true;
+                AddComponent(new Coroutine(OnCollected((Player)entity)));
+            }
+        }
+
+        public abstract IEnumerator OnCollected(Player player);
+    }
+}
---
+++ Triggers/PlayerTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_platformer
+{
+    public abstract class PlayerTrigger : Trigger
+    {
+        public PlayerTrigger(Vector2 position, Vector2 size, Sprite sprite)
+            : base(position, (int)size.X, (int)size.Y, new List<Type> { typeof(Player) }, sprite) { }
+
+        public PlayerTrigger(Vector2 position, int width, int height, Sprite sprite)
+            : base(position, new Vector2(width, height), new List<Type> { typeof(Player) }, sprite) { }
+
+        public PlayerTrigger(Rectangle triggerRect, Sprite sprite)
+            : base(triggerRect.Location.ToVector2(), triggerRect.Size.ToVector2(), new List<Type> { typeof(Player) }, sprite) { }
+    }
+}
