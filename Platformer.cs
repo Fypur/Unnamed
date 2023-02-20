a6d15a6ab592d134c1b23e5f37884b1035a81a7b
catapillie <espritox@gmail.com>
2023-02-20 22:34:06 +0100

    new glitch screen sprite animation

--- Platformer.cs
+++ Platformer.cs
@@ -31,7 +31,7 @@ namespace Platformer
         public static Tile BackgroundTile;
 
 #if DEBUG
-        public static string InitLevel = "60";
+        public static string InitLevel = "83";
         public static int InitWorld = 0;
         private FileSystemWatcher watcher;
         private bool waitRefresh;
