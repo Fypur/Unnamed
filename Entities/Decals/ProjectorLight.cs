23b149ae2ce56c97a2fd9b3e7806e2d1e0aaec71
Fypur <84095553+Fypur@users.noreply.github.com>
2023-02-27 00:19:26 +0100

    More Decoration

--- Entities/Decals/ProjectorLight.cs
+++ Entities/Decals/ProjectorLight.cs
@@ -38,14 +38,6 @@ namespace Platformer
         {
             base.Render();
 
-            //Pos = Input.MousePos;
-
-            /*var r1 = new Raycast(Raycast.RayTypes.MapTiles, Pos + lightRect.Location.ToVector2() + new Vector2(lightRect.Width, 0), VectorHelper.RotateDeg(Vector2.UnitY, -45), 300f);
-            var r2 = new Raycast(Raycast.RayTypes.MapTiles, Pos + lightRect.Location.ToVector2(), VectorHelper.RotateDeg(Vector2.UnitY, 45), 300f);
-            collidedPoint = r1.EndPoint;
-            lightIntensity = (int)(r1.Distance / 1.2f);
-            collidedPoint2 = r2.EndPoint;
-            lightIntensity2 = (int)(r2.Distance / 1.2f);*/
 
             Drawing.DrawQuad(
                 Pos + lightRect.Location.ToVector2(), new Color(Color.White, 70),
--- Platformer.cs
+++ Platformer.cs
@@ -31,7 +31,7 @@ namespace Platformer
         public static Tile BackgroundTile;
 
 #if DEBUG
-        public static string InitLevel = "85";
+        public static string InitLevel = "61";
         public static int InitWorld = 0;
         private FileSystemWatcher watcher;
         private bool waitRefresh;
